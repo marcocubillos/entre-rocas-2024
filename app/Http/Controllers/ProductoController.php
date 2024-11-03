230dbcd03ff02a0d19a847d67d7b6987ef39efca
marco cubillos <antonio.cubillos1603@gmail.com>
2024-11-03 15:14:37 -0500

    APK desarrollar módulos móvil según requerimientos del proyecto

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -9,43 +9,31 @@ class ProductoController extends Controller
 {
 
     public function index()
-{
+    {
     // Obtener todos los productos de la base de datos
     $productos = Producto::all();
 
     // Retornar la vista 'productos.index' y pasar los productos a la vista
     return view('productos.index', compact('productos'));
-}
+    }
 
 
 
     public function create()
-    {
-        //
+    { 
+    return view("productos.create");
     }
 
 
     public function store(Request $request)
     {
-        // Validar Productos 
-        $datos = $request->validate([
-       'nombre' =>['required', 'string', 'max:100'],
-       'descripcion' =>['nullable','string', 'max:255'],
-       'precio' =>['required', 'integer', 'min:1000'],
-       'stock' =>['required', 'integer','min:1'], 
-        ]);
-       
-
-     //Guardar Datos 
- $producto = Producto::create($datos); 
-
- // Respuesta al Cliente 
-return response()->json(['success' => true, 'message' => 'Producto creado'], 201); 
- }
+        producto::create($request->all());
+        return redirect()->route("productos.index");
+    }
 
     public function show(Producto $producto)
     {
-        return response()->json($producto, 200); //200: OK
+        //
     }
 
     /**
@@ -53,31 +41,20 @@ return response()->json(['success' => true, 'message' => 'Producto creado'], 201
      */
     public function edit(Producto $producto)
     {
-        //
+        return view("productos.edit", compact("producto"));
     }
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Producto $producto)
+
     {
-        // Validar datos de entrada
-        $datos = $request->validate([
-       'nombre' =>['required', 'string','max:100'],
-       'descripcion' =>['nullable','string', 'max:255'],
-       'precio' =>['required', 'integer','min:1000'],
-       'stock' =>['required', 'integer','min:1'], 
-        ]);
-        // Actualizar Producto
-        $producto->update($datos);
-        // Respuesta al Cliente
-        return response()->json(['success' => true,'message' => 'Producto actualizado'], 200); 
-        }
+        $producto->update($request->all());
+    }
     public function destroy(Producto $producto)
     {
-        // Eliminar Producto
         $producto->delete();
-        // Respuesta al Cliente
-        return response()->json(['success' => true,'message' => 'Producto eliminado'], 200);
-        }
+        return redirect()->route("productos.index");
+    }
 }
--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -3,9 +3,14 @@
 @section("titulo", "pagina principal")
 
 @section("contenido")
+  {{-- Botón para crear un producto nuevo --}}
+ <div class="flex justify-end m-4">
+    <a href="{{ route('productos.create') }}" class="btn btn-outline">Nuevo producto</a>
+  </div>
 
+  <div class= "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-6">
     @foreach($productos as $producto)
-    <div class="card bg-base-100 image-full w-96 shadow-xl">
+    <div class="card bg-base-100 w-96 shadow-xl m-8">
         <figure>
           <img
             src="https://picsum.photos/id/{{ $producto->id }}/240"
@@ -14,7 +19,15 @@
         <div class="card-body">
           <h2 class="card-title">{{ $producto->nombre }}</h2>
           <p>{{ $producto->descripcion }}</p>
+          precio: <div class="badge badge-outline">{{ $producto-> precio}}</div>
+          Stock: <div class="badge badge-outline">{{ $producto-> stock}}</div>  
           <div class="card-actions justify-end">
+            <a href="{{ route('productos.edit', $producto->id) }}" class="btn btn-outline btn-xs" >Editar</a>
+            <form action="{{ route('productos.destroy', $producto->id) }}" method="POST">
+              @csrf
+              @method('DELETE')
+              <button type="submit" class="btn btn-outline btn-xs">Eliminar</button>
+             </form>
             <button class="btn btn-primary">Buy Now</button>
           </div>
         </div>
